47dc10042658ddb272a15da7c5d507777ecf0d2d
Gelmi <ggelmisalvo@gmail.com>
2020-04-27 18:08:48 -0300

    adding more images

--- src/screens/quizscreen/index.js
+++ src/screens/quizscreen/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import Questoes from './../../data/questions.json';
 
 // import { Container } from './styles';
 
+function randomInt(max) {
+	return Math.floor((max) * Math.random());
+}
+
 export default function quizscreen() {
+
+  function generateQuiz() {
+
+  };
+
   return (
-    <View style={styles.container}/>
+    <View style={styles.container}>
+    <TouchableOpacity 
+        style={styles.startButton}
+    >
+        <Text style={styles.startButtonText}>Teste</Text>
+    </TouchableOpacity>
+</View>
   );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#121212',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  startButton: {
+      backgroundColor: '#bb86fc',
+      width: '30%',
+      height: '5%',
+      borderRadius: 3,
+      alignItems: "center",
+      justifyContent: "center"
+  },
+  startButtonText: {
+      fontWeight: "700",
+      fontSize: 15
+  },
+});
+
   
